da2a0e43dc6cbf67f757e45c19a8d18ebb790254
NoSoyVerde <hectorkotor@gmail.com>
2026-01-12 10:22:20 +0100

    ResourceNotFound electric boogalo

--- gesportin/src/main/java/net/ausiasmarch/gesportin/exception/ResourceNotFoundException.java
+++ gesportin/src/main/java/net/ausiasmarch/gesportin/exception/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
 package net.ausiasmarch.gesportin.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
 }
