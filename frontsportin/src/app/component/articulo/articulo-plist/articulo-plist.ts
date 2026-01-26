2b2c53a2700104ab460dae83e36d2a530c03df11
Alejandro Pavon <alejandropm06@gmail.com>
2026-01-26 13:00:34 +0100

    Creación de artículo

---
+++ frontsportin/src/app/component/articulo/articulo-plist/articulo-plist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articulo-plist',
+  imports: [],
+  templateUrl: './articulo-plist.html',
+  styleUrl: './articulo-plist.css',
+})
+export class AdminPlist {
+
+}
