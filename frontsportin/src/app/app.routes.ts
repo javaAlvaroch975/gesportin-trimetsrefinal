2c673645307f06d4faaa2d92cc71a4b3da54251a
Linztark <anasilvesttttre@gmail.com>
2026-01-29 12:08:55 +0100

    plist rolusuario

--- frontsportin/src/app/app.routes.ts
+++ frontsportin/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
 import { ArticuloPlistAdminRouted } from './component/articulo/plist-admin-routed/articulo-plist';
+import { RolusuarioPlist } from './component/rolusuario/plist-admin-routed/rolusuario-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'articulo', component: ArticuloPlistAdminRouted},
-    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted}    
+    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted}  ,
+    { path: 'rolusuario', component: RolusuarioPlist },  
 ];
