496c1b04f2dbe6984b844d01023fe30b0e37942f
Alan <alanalcaniz24@gmail.com>
2026-02-01 21:21:54 +0100

    plist filtros, manytoone y onetomany

--- frontsportin/src/app/app.routes.ts
+++ frontsportin/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
 import { ArticuloPlistAdminRouted } from './component/articulo/plist-admin-routed/articulo-plist';
+import { NoticiaPlistAdminRouted } from './component/noticia/plist-admin-routed/noticia-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'articulo', component: ArticuloPlistAdminRouted},
-    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted}    
+    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted},
+    { path: 'noticia', component: NoticiaPlistAdminRouted},
+    { path: 'noticia/:club', component: NoticiaPlistAdminRouted}
 ];
