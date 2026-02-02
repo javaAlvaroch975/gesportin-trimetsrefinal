6c8df783788a2b0b400a43cee0e368cd56445883
Joan_Salinas <joansalinasf@gmail.com>
2026-02-02 09:53:38 +0100

    Hecho

--- frontsportin/src/app/app.routes.ts
+++ frontsportin/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
 import { ArticuloPlistAdminRouted } from './component/articulo/plist-admin-routed/articulo-plist';
+import { FacturaPlistAdminRouted } from './component/factura/plist-admin-routed/factura-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'articulo', component: ArticuloPlistAdminRouted},
-    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted}    
+    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted},{ path: 'factura', component: FacturaPlistAdminRouted },
+    { path: 'factura/:usuario', component: FacturaPlistAdminRouted }    
 ];
