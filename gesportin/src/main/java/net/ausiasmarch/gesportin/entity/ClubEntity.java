e1783022c0232a4cf9356ca2857f18da35e08653
Rafael Aznar <rafaaznar@gmail.com>
2026-03-13 18:05:40 +0100

    Add JsonFormat annotation to fechaAlta in ClubEntity for consistent date formatting

--- gesportin/src/main/java/net/ausiasmarch/gesportin/entity/ClubEntity.java
+++ gesportin/src/main/java/net/ausiasmarch/gesportin/entity/ClubEntity.java
@@ -3,6 +3,8 @@ package net.ausiasmarch.gesportin.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -39,6 +41,7 @@ public class ClubEntity {
     private String telefono;
 
     @Column(name = "fecha_alta", nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime fechaAlta;
 
     @Lob
